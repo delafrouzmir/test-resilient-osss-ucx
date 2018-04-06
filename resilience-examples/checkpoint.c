c702e9926232f55ac31f1bc392389ba3368076cc
delafouzmir <delafrooz.mir16@gmail.com>
2018-04-06 16:53:48 -0400

    new chechkpointing. removing MSPEs

--- resilience-examples/checkpoint.c
+++ resilience-examples/checkpoint.c
@@ -5,104 +5,156 @@
 
 #define ORIGINAL_PE     0
 #define SPARE_PE        1
-#define MSPE            2
-#define RESURRECTED_PE  3
-#define DEAD_PE         4
+//#define MSPE            2
+#define RESURRECTED_PE  2
+#define DEAD_PE         3
 
 #define SUCCESS     0
 #define FAILURE     1
 
-#define MAX_NUM_MSPE    2
+//#define MAX_NUM_MSPE    2
 
 #define NO_CHECKPOINT           0
+//maybe change the collective name, since it's not collective?
 #define COLLECTIVE_CHECKPOINT   1
 #define RING_CHECKPOINT         2
 
+#define MAX_CARRIER_SIZE        1000
 
-void **shadow_mem;
-void ***checkpoint_table;
-int shadow_mem_size, pe_type, num_opes, checkpointing_mode;
+
+// names starting with shmem_cpr_ are reserved for functions
+// names starting with cpr_ are reserved for variable declarations
+typedef struct carrier {
+    int id;                     // ID of memory part that is being checkpointed
+    int count;                  // number of data items that needs to be stored
+    int mess[MAX_CARRIER_SIZE]; // an array of data that will be stored
+} cpr_carrier;
+
+void **cpr_shadow_mem;
+void ***cpr_checkpoint_table;
+int cpr_shadow_mem_size, cpr_pe_type, cpr_num_opes, cpr_checkpointing_mode;
+int *cpr_pe;
+cpr_carrier *mess;
 
 
 void shmem_cpr_set_pe_type (int me, int npes, int spes)
 {
-
     /*
-    * (npes-spes) PEs are the original ones, the rest:
-        * If the nummber of spare PEs is less than or equal to 2,
-        * then all of the spare PEs are Main Spare PEs (MSPE).
-        * Else, 2 of spare PEs are MSPEs, the rest are just spares.
+    * TO DO:
+    * add checkpointing mode to arguments
+    */
+    /*
+    * PEs 0 to npes-spes-1 are originals
+    * the rest are spares
     */
     if ( me >= 0 && me < npes - spes )
-        pe_type = ORIGINAL_PE;
+        cpr_pe_type = ORIGINAL_PE;
+    else
+        cpr_pe_type = SPARE_PE;
+
+    cpr_num_opes = npes - spes;
+    int *cpr_pe = (int *) shmem_malloc (cpr_num_opes * sizeof(int));
+
+    // instead of pe number i, we should use cpr_pe[i]
+    // e.g: shmem_put(dest, &source, nelems, cpr_pe[j]);
+    int i=0;
+    for (; i<cpr_num_opes; ++i)
+        cpr_pe[i] = i;
+    
+    /*
+    * This part is for case of having MSPEs
     else
     {
         if ( spes <= MAX_NUM_MSPE )
-            pe_type = MSPE;
+            cpr_pe_type = MSPE;
         else
         {
             if ( me >= npes - spes && me < npes - spes + MAX_NUM_MSPE )
-                    pe_type = MSPE;
+                    cpr_pe_type = MSPE;
             else
-                pe_type = SPARE_PE;
+                cpr_pe_type = SPARE_PE;
         }
     }
+    */
 }
 
 
-int shmem_cpr_init (int me, int npes, int spes)
+void shmem_cpr_spare_wait (int me, int npes, int spes)
+{
+    int ;
+
+}
+
+
+int shmem_cpr_init (int me, int npes, int spes, int mode)
 {
     /*
     * Incorrect input
     */
     if ( spes == 0 || spes > npes-1 || me < 0 || me >= npes )
     {
-        checkpointing_mode = NO_CHECKPOINT;
+        cpr_checkpointing_mode = NO_CHECKPOINT;
         return FAILURE;
     }
 
-    num_opes = npes - spes;
+    mess = (cpr_carrier *) shmem_malloc(1* sizeof(cpr_carrier));
+
+    // add an if for different checkpointing mode here
+    // if (  )
+    cpr_checkpointing_mode = COLLECTIVE_CHECKPOINT;
     shmem_cpr_set_pe_type (me, npes, spes);
 
-    switch (pe_type)
+    /*
+    * ORIGINAL or RESURRECTED PEs have a shadow_mem for chckpointing their own data
+    * SPARE PEs have a checkpoint_table which is a copy of all
+    *    ORIGINAL or RESURRECTED PEs' shadow_mem's
+    */
+    switch (cpr_pe_type)
     {
         case ORIGINAL_PE:
-            shadow_mem_size = 1;
-            shadow_mem = (void **) malloc(shadow_mem_size * sizeof(void *) );
+            cpr_shadow_mem_size = 1;
+            cpr_shadow_mem = (void **) malloc(cpr_shadow_mem_size * sizeof(void *) );
             break;
 
         case SPARE_PE:
-            // can we add something here that prevents spares
-            // from running the code between shmem_cpr_init and finalize as others?
-            break;
-
-        case MSPE:
-            checkpoint_table = (void ***) malloc(num_opes * sizeof(void **));
+            cpr_checkpoint_table = (void ***) malloc(cpr_num_opes * sizeof(void **));
             int i;
-            for (i=0; i<num_opes; ++i)
+            for (i=0; i<cpr_num_opes; ++i)
             {
-                checkpoint_table[i] = (void **) malloc (1 * sizeof(void**));
+                cpr_checkpoint_table[i] = (void **) malloc (1 * sizeof(void**));
             }
             break;
-
+        /*
+        case MSPE:
+            cpr_checkpoint_table = (void ***) malloc(cpr_num_opes * sizeof(void **));
+            int i;
+            for (i=0; i<cpr_num_opes; ++i)
+            {
+                cpr_checkpoint_table[i] = (void **) malloc (1 * sizeof(void**));
+            }
+            break;
+        */
         default:
             // Nothing here for now, but it may change "if" init is called for subtitute PEs
             break;
     }
 
     shmem_barrier_all();
+
+    if ( cpr_pe_type == SPARE_PE )
+        shmem_cpr_spare_wait(me, npes, spes);
     return SUCCESS;
 }
 
 int shmem_cpr_reserve (int id, int * mem, int count, int pe_num)
 {
     /* TO DO:
-    1- create a hash table for id s and the index in shadow_mem or checkpoint_table
+    1- create a hash table for id s and the index in cpr_shadow_mem or cpr_checkpoint_table
     2- generalize from int to type ( ==> type * mem)
     3- Q: I don't want the checkpointing to be collective
     if it's not collective, then how do spares and MSPEs call these functions?
     is there a way to trigger a function in one PE from another? (send a message, wait_until, ...)
-    if it's not collective, how should MSPE know which PEs want to reserve? (not update all the indices of checkpoint_table)
+    if it's not collective, how should MSPE know which PEs want to reserve? (not update all the indices of cpr_checkpoint_table)
     if it's collective, then how to stop spares from checkpointing empty memory?
     4- how to stop spares to do anything from init to finalize at all?
     5- collective or not: what to do with pe_num (the pe that called this function)?
@@ -111,42 +163,34 @@ int shmem_cpr_reserve (int id, int * mem, int count, int pe_num)
     8- hashtable: id should be unique. look up id. if it already exists, error for user
     */
 
-    // for now, I assume id == index or id == shadow_mem_size-1
-    switch (pe_type)
+    // for now, I assume id == index or id == cpr_shadow_mem_size-1
+    switch (cpr_pe_type)
     {
         case ORIGINAL_PE:
         case RESURRECTED_PE:
-            shadow_mem_size ++;
-            shadow_mem = (void **) realloc (shadow_mem_size * sizeof(void *) );
-            shadow_mem[id] = (void *) malloc (count * sizeof(int));
+            cpr_shadow_mem_size ++;
+            cpr_shadow_mem = (void **) realloc (cpr_shadow_mem_size * sizeof(void *) );
+            cpr_shadow_mem[id] = (void *) malloc (count * sizeof(int));
             int i;
             for ( i=0; i < count; ++i )
-                shadow_mem[id][i] = mem[i];
+                cpr_shadow_mem[id][i] = mem[i];
             // update hashtable
             break;
 
         case SPARE_PE:
-            // nothing for now
+            
             break;
 
+        /*
         case MSPE:
-            // TO DO:
-            // collective or not :((((((((
-            // this for loop should not be from 0 to opes
-            // it should be for all original or resurrected ones
-            // and their number may not be in order
-
-            // if not collective: get the shadow_mem_size from pe pe_num (= pe_sh_size)
-            // realloc checkpoint_table[pe_num] to one size bigger
-            // copy mem to checkpoint_table[pe_num][id]
             int i;
             // if collective, we need to iterate on all original or resurrecteds in below
-            for (i=0; i<num_opes; ++i)
+            for (i=0; i<cpr_num_opes; ++i)
             {
-                checkpoint_table[pe_num] = (void **) malloc (1 * sizeof(void**));
+                cpr_checkpoint_table[pe_num] = (void **) malloc (1 * sizeof(void**));
             }
             break;
-
+        */
         default:
             // Nothing here for now, but it may change "if" init is called for subtitute PEs
             break;
@@ -161,16 +205,16 @@ int shmem_cpr_checkpoint ( int id, void* mem, int size, int pe_num )
     lookup id in hashtable to get the index
     update the index-th element in
         original or ressurected: shadow mem
-        MSPE: checkpoint_table[pe_num][index]
+        MSPE: cpr_checkpoint_table[pe_num][index]
     */
     // for now, I assume id = index
-    switch (pe_type)
+    switch (cpr_pe_type)
     {
         case ORIGINAL_PE:
         case RESURRECTED_PE:
             int i;
             for ( i=0; i < count; ++i )
-                shadow_mem[id][i] = mem[i];
+                cpr_shadow_mem[id][i] = mem[i];
             break;
 
         case SPARE_PE:
@@ -193,21 +237,22 @@ int shmem_cpr_restore ( int pe_num )
     lookup id in hashtable to get the index
     update the index-th element in
         original or ressurected: shadow mem
-        MSPE: checkpoint_table[pe_num][index]
+        MSPE: cpr_checkpoint_table[pe_num][index]
     */
-    switch (pe_type)
+    switch (cpr_pe_type)
     {
         case ORIGINAL_PE:
         case RESURRECTED_PE:
-            //shadow_mem_size ++;
+            //cpr_shadow_mem_size ++;
             break;
 
         case SPARE_PE:
             break;
 
+        /*
         case MSPE:
             break;
-
+        */
         default:
         // nothing here for now
             break;
@@ -237,7 +282,7 @@ int main ()
     int res = shmem_cpr_init(me,npes, spes);
     if ( me == 0 )
         printf ("init is %d\n", res);
-    printf("I am %d with pe_type= %d\n", me, pe_type);
+    printf("I am %d with cpr_pe_type= %d\n", me, cpr_pe_type);
 
     /*
     int *a = (int *) malloc(10*sizeof(int));
