69f55a6f7381774ab5e8215e27f732cee3c2bb9f
delafouzmir <delafrooz.mir16@gmail.com>
2019-05-05 16:28:30 -0400

    testing pe role and type, shmem_malloc not on global variables, c11

--- resilience-examples/test_shmem_malloc_on_global.c
+++ resilience-examples/test_shmem_malloc_on_global.c
@@ -18,6 +18,14 @@ int main(int argc, char const *argv[])
 	for ( i=0; i<npes; ++i )
 		spr_pes[i] = me;
 
+	for ( i=0; i<npes; ++i )
+		shmem_int_put ( me, &spr_pes[i], 1, (me+1)%npes);
+
+	if ( me == 0 )
+		for ( i=0; i<npes; ++i )
+			printf("%d ",spr_pes[i]);
+
+	printf("\n");
 	shmem_finalize();
 	return 0;
 }
