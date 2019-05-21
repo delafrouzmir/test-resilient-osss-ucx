4713bf7d091ca1efb8c86890abc34c13bc45df16
delafouzmir <delafrooz.mir16@gmail.com>
2019-05-21 14:40:34 -0400

    testing communication in cpr_table_copy

--- resilience-examples/2cp_rb_matmul.c
+++ resilience-examples/2cp_rb_matmul.c
@@ -711,7 +711,9 @@ int shmem_cpr_copy_check_table ( int candid, int storage, int pe_num )
     int i, j, k, l, space_needed, last_data, q_tail;
     cpr_check_carrier *carr;
     cpr_sig_table_info = 0;
-    cpr_table_info_carr = 0;
+    cpr_table_info_size = 0;
+    cpr_table_info_tail = 0;
+    cpr_table_info_num_carr = 0;
 
     if ( pe_num == candid )
     {
